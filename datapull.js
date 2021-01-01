b9853a3474dfef413a42340000fb24505ac72f48
SightEyeFG <75752611+SightEyeFG@users.noreply.github.com>
2021-01-01 18:54:30 -0500

    Update datapull.js

--- datapull.js
+++ datapull.js
@@ -1,9 +1,21 @@
-var settings = {
-    "url": "api.coincap.io/v2/assets",
-    "method": "GET",
-    "timeout": 0,
-};
-
 $.ajax(settings).done(function(response) {
     console.log(response);
 });
+
+
+const api_url = 'api.coincap.io/v2/assets';
+
+let firstTime = true;
+
+async function getISS() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+
+
+
+}
+
+getISS();
+
+setInterval(getISS, 1000);
+console.log(response);
